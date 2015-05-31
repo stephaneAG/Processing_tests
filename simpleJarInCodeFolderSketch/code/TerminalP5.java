31b888f0a79972986db1ebdcd505d2e0a068b684
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-05-31 04:22:10 +0200

    Create TerminalP5.java

---
+++ simpleJarInCodeFolderSketch/code/TerminalP5.java
@@ -0,0 +1,20 @@
+/**
+*	A simple Java Librairy for Processing
+*	adding terminal commands from Pr. & their Logs
+*	
+*	by Stéphane Adam Garnier - 2012
+*/
+
+package terminalP5; // package name
+
+//import processing.core.*;
+
+public class TerminalP5{ // "class container" / 'package'
+	
+	public static void main(String[] args){ // "main"
+		
+		System.out.println("TerminalP5 v1a - a Processing lib by Stéphane Adam Garnier - 2012");
+
+	}
+
+}
